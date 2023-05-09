9a0cea567868eef1a22f2f0cb8ecf49d5f12d942
MDMwakulo <mdmwakulo@gmail.com>
2023-05-09 14:40:43 +0300

    Truthy and Falsy Values

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ John's BMI (${markBMI2}) is higher than Mark's (${johnBMI2}).`);
 }*/
 
 // type conversion
-const inputYear = '1997';
+/*const inputYear = '1997';
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
 
@@ -137,4 +137,27 @@ let n = '1' + 1; // '11'
 n = n - 1; // "10"
 console.log(n);
 
-console.log('10' - '4' - '3' - 2 + '5');
+console.log('10' - '4' - '3' - 2 + '5');*/
+
+// falsy values
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean('durell'));
+console.log(Boolean(undefined));
+console.log(Boolean({}));
+
+let money = 0;
+
+if (money) {
+	console.log("Don't spend it all ;)");
+} else {
+	console.log('You should get a job');
+}
+
+let height;
+
+if (height) {
+	console.log('YAY! Height is defined');
+} else {
+	console.log('Height is UNDEFINED');
+}
